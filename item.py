96b7e216b88a7f3299942d04cdfc1b97fb12e12b
KikaDIEN1994 <dien.kika@gmail.com>
2021-09-10 11:36:05 +0200

    Add comment item.py

--- item.py
+++ item.py
@@ -1,17 +1,20 @@
 class Item:
+    """In this class we going to define each objet to pick"""
+
     def __init__(self, x, y, image):
+    #init picture of items
         self.x = x
         self.y = y
         self.is_drop = False
         self.image = image
 
     def get_position(self):
+    # get position items
         return (self.x, self.y)
-        
+
     def is_item_drop(self):
         return self.is_drop
-        
+
     def drop_item(self):
         self.is_drop = True
-        print("item")
-        #return self.is_drop
+
